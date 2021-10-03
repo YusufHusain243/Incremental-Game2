d7511a556c3aabd3772630218000786974fa6cf4
husainyusuf382@gmail.com <husainyusuf382@gmail.com>
2021-10-03 15:25:37 +0700

    First Commit

---
+++ Assets/Scripts/PagesManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PagesManager : MonoBehaviour
+{
+    void Update()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        if (Input.GetKey(KeyCode.Escape)){
+            if(sceneName == "Game 1")
+            {
+                SceneManager.LoadScene("Loading");
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public float speed;
+    public Rigidbody2D rb;
+    public Vector3 startPosition;
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        startPosition = transform.position;
+        Launch();
+    }
+
+    public void Launch()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb.AddForce(new Vector2(speed * x, speed * y));
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        audio.Play();
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
